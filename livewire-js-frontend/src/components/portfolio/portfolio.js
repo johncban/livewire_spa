dd31428f7a5219d684882815689e1f9b4216553d
johncban <johncban@gmail.com>
2021-04-25 21:12:11 -0400

    Refractor Portfolios

--- livewire-js-frontend/src/components/portfolio/portfolio.js
+++ livewire-js-frontend/src/components/portfolio/portfolio.js
@@ -9,7 +9,7 @@ class Portfolio {
 
   renderNewStockP() {
     this.psForm = document.getElementById('p-category')
-    window.localStorage.setItem('p_id', this.id)
+    //window.localStorage.setItem('p_id', this.id)
     this.psForm.innerHTML += `
     <option value="" disabled selected></option>
     <option id="${this.id}" value="${this.id}">${this.p_name}</option>`
@@ -26,13 +26,13 @@ class Portfolio {
     location.reload(true)
   }
 
-  removeStock(stockId) {
-    this.stocks = this.stocks.filter(stock => stock.s_id !== stockId)
+  removeStock(sId) {
+    this.stocks = this.stocks.filter(stock => stock.s_id !== sId)
   }
 
 
   render() {
-    window.localStorage.setItem('p_id', this.id)
+    //window.localStorage.setItem('p_id', this.id)
     return `<li data-pid='${this.id}' data-props='${JSON.stringify(this)}' class='ps-element'>
       <button class='btn_m_edit' data-action='edit-p'>Edit</button>
       <button class='btn_m_del' data-action='delete-p'>Delete</button>
